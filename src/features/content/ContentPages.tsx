b5e6bce64f97f51658b5510ef8c9317a31299b8c
nunomi0 <ykys1@naver.com>
2026-08-25 22:27:41 +0900

    Fix: 나가기 화살표 복구

--- src/features/content/ContentPages.tsx
+++ src/features/content/ContentPages.tsx
@@ -2462,6 +2462,7 @@ export function ContentInspectionDetailPage() {
           onClick={() => setExitConfirmationOpen(true)}
           type="button"
         >
+          <ArrowLeft aria-hidden="true" size={16} />
           나가기
         </button>
         {content ? (
